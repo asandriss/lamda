035c756f11a7b2f907d79290e7e4645dee77df56
Fedor Hajdu <fedorhajdu@gmail.com>
2017-01-23 16:21:01 +0400

    Introduce DataFrame API and imporve logging

--- spark-lambda/src/main/scala/batch/BatchJob.scala
+++ spark-lambda/src/main/scala/batch/BatchJob.scala
@@ -1,9 +1,11 @@
 package batch
 
 import java.lang.management.ManagementFactory
-import org.apache.spark.{SparkContext, SparkConf}
+
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.log4j.{Level, Logger}
 import domain._
+import org.apache.spark.sql.SQLContext
 /**
   * Created by Fedor.Hajdu on 12/22/2016.
   */
@@ -19,47 +21,48 @@ object BatchJob {
       System.setProperty("hadoop.home.dir", "D:\\HadoopUtils\\hadoop-common-2.2.0-bin-master")
       conf.setMaster("local[*]")
     }
+    Logger.getLogger("org").setLevel(Level.OFF);
+    Logger.getLogger("akka").setLevel(Level.OFF);
 
     // setup spark context
     val sc= new SparkContext(conf)
     val rootLogger = Logger.getRootLogger()
-    Logger.getLogger("org").setLevel(Level.OFF);
-    Logger.getLogger("akka").setLevel(Level.OFF);
     rootLogger.setLevel(Level.ERROR)
+
+    implicit val sqlContext = new SQLContext(sc)
+
+    import org.apache.spark.sql.functions._
+    import sqlContext.implicits._
 
     // initialize RDD
     val sourceFile = "file:///d:/tmp/spark-test/data.tsv"
     val input = sc.textFile(sourceFile)
-    val inputRdd = input.flatMap { line =>
+
+    // Change RDD to data frame
+    val inputDF = input.flatMap { line =>
       val record = line.split("\\t")
       val ms_in_hour = 1000*60*60
       if(record.length == 7)
-        Some(Activity(record(0).toLong / ms_in_hour, record(1), record(2), record(3), record(4), record(5), record(6)))
+        Some(Activity(record(0).toLong / ms_in_hour * ms_in_hour, record(1), record(2), record(3), record(4), record(5), record(6)))
       else
         None
-    }
+    }.toDF()
 
-    val keyedByProduct = inputRdd.keyBy(a => (a.product, a.timestamp_hour)).cache()
-    val visitorsByProduct = keyedByProduct
-      .mapValues(p => p.visitor)
-      .distinct()
-      .countByKey()     // helper for reduce action
+    val df = inputDF.select(
+      //add_months(inputDF("timestamp_hour"), 1).as("timestamp_hour"),
+      inputDF("timestamp_hour"),
+      inputDF("referrer"), inputDF("action"), inputDF("prevPage"), inputDF("page"), inputDF("visitor"),inputDF("product")
+    ).cache()
 
-    val activityByProduct = keyedByProduct    // will use cached version now
-        .mapValues { p => // create a map of actions
-          p.action match {
-            case "purchase" => (1, 0, 0)
-            case "add_to_cart" => (0, 1, 0)
-            case "page_view" => (0, 0, 1)
-          }
-        }
-        //.reduceByKey( (a, b) => (a._1 + b._1,a._2 + b._2,a._3 + b._3)) // reduce action sum the bits to get actual counts
+    df.registerTempTable("activity")
 
-    println("Visitors By Product:")
+    val visitorsByProduct = sqlContext.sql(
+      """SELECT product, timestamp_hour, COUNT(DISTINCT visitor) as unique_visitors
+        |FROM activity
+        |GROUP BY product, timestamp_hour
+      """.stripMargin)
+
+    visitorsByProduct.printSchema()
     visitorsByProduct.foreach(println)
-
-    println("*****************************************************************************************")
-    println("Activity by Product:")
-    activityByProduct.foreach(println)
   }
 }
